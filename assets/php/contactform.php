86732c1912f1089e5132c43e8fe26e6dc03936eb
Lubomir Zubko <32295327+lubozubko@users.noreply.github.com>
2020-01-22 19:21:39 +0000

    added reCAPTCHA code and changed form code

--- assets/php/contactform.php
+++ assets/php/contactform.php
@@ -1,15 +1,57 @@
+//Coded by Lubomir Zubko | www.zubko.co.uk
+
 <?php
 
-if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $subject = $_POST['subject'];
-    $mailFrom = $_POST['mail'];
-    $message = $_POST['message'];
 
-    $mailTo = "zubko.lubomir@gmail.com";
-    $headers = "From: ".$mailFrom;
-    $txt = "You have received an e-mail from ".$name."."\n\n".$message;
+//reCAPTCHA
+// Checks if form has been submitted
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    function post_captcha($user_response) {
+        $fields_string = '';
+        $fields = array(
+            'secret' => '6Ldsg9EUAAAAADyRXPuZSubDO08wPQAZoU4BDwzM',
+            'response' => $user_response
+        );
+        foreach($fields as $key=>$value)
+        $fields_string .= $key . '=' . $value . '&';
+        $fields_string = rtrim($fields_string, '&');
 
-    mail($mailTo, $subject, $txt, $headers);
-    header("Location: index.php?mailsend");
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://www.google.com/recaptcha/api/siteverify');
+        curl_setopt($ch, CURLOPT_POST, count($fields));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, True);
+
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        return json_decode($result, true);
+    }
+
+    // Call the function post_captcha
+    $res = post_captcha($_POST['g-recaptcha-response']);
+
+    if (!$res['success']) {
+        // What happens when the CAPTCHA wasn't checked
+        echo '<p>Please go back and make sure you check the security CAPTCHA box.</p><br>';
+    } else {
+        // If CAPTCHA is successfully completed...
+
+        // form
+        //if (isset($_POST['submit'])) {
+            $name = $_POST['name'];
+            $subject = $_POST['subject'];
+            $mailFrom = $_POST['mail'];
+            $message = $_POST['message'];
+
+            $mailTo = "info@hceating.com";
+            $headers = "From: ".$mailFrom;
+            $txt = "You have received an e-mail from " .$name. " . \n\n".$message;
+
+            mail($mailTo, $subject, $txt, $headers);
+            //header("Location: index.php?mailsend");
+            echo '<p>Your email has been sent.</p><br>';
+
+        //}
+    }
 }
